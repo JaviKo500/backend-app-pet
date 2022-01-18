6768150f0eb078add76feb2c701e8d06085604eb
JaviKo500 <javikogutierrez64@gmail.com>
2022-01-18 09:02:17 -0500

    first commit

---
+++ src/main/java/com/back/app/pet/AppPetApplication.java
@@ -0,0 +1,13 @@
+package com.back.app.pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back/app/pet/AppPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.app.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
